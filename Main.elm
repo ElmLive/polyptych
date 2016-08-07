59724b5f309e3f882fe2192443b67ade659a412f
Aaron VonderHaar <gruen0aermel@gmail.com>
2016-08-07 12:02:47 -0700

    Refactor showing a single image

--- Main.elm
+++ Main.elm
@@ -6,12 +6,27 @@ import Html.Attributes
 
 
 type alias Model =
-    ()
+    { canvas : Size
+    , frame : Frame
+    }
+
+
+type alias Size =
+    { width : Int, height : Int }
+
+
+type Frame
+    = SingleImage { url : String }
 
 
 initialModel : Model
 initialModel =
-    ()
+    { canvas = { width = 250, height = 250 }
+    , frame =
+        SingleImage
+            { url = "https://i.imgur.com/gt5lnkS.jpg"
+            }
+    }
 
 
 type Msg
@@ -23,24 +38,32 @@ update msg model =
     ( model, Cmd.none )
 
 
-viewCanvas : Html.Html Msg
-viewCanvas =
+viewCanvas : Size -> Frame -> Html.Html Msg
+viewCanvas size rootFrame =
     Html.div
         [ Html.Attributes.style
-            [ ( "width", "250px" )
-            , ( "height", "250px" )
+            [ ( "width", toString size.width ++ "px" )
+            , ( "height", toString size.height ++ "px" )
             , ( "border", "2px solid black" )
             ]
         ]
-        [ Html.div
-            [ Html.Attributes.style
-                [ ( "height", "250px" )
-                , ( "background-image", "url(https://pixabay.com/static/uploads/photo/2015/10/18/11/58/beetles-994211_960_720.jpg)" )
-                , ( "background-size", "auto 250px" )
-                ]
-            ]
-            []
+        [ viewFrame size rootFrame
         ]
+
+
+viewFrame : Size -> Frame -> Html.Html Msg
+viewFrame size frame =
+    case frame of
+        SingleImage { url } ->
+            Html.div
+                [ Html.Attributes.style
+                    [ ( "height", toString size.height ++ "px" )
+                    , ( "width", toString size.width ++ "px" )
+                    , ( "background-image", "url(" ++ url ++ ")" )
+                    , ( "background-size", "auto " ++ toString size.height ++ "px" )
+                    ]
+                ]
+                []
 
 
 view : Model -> Html.Html Msg
@@ -48,7 +71,7 @@ view model =
     Html.div
         [ Html.Attributes.style [ ( "padding", "8px" ) ]
         ]
-        [ viewCanvas
+        [ viewCanvas model.canvas model.frame
         , Html.hr [] []
         , Html.text <| toString model
         ]
